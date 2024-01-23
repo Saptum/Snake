5ab00e0f82f363215fa8920f2a0c712c067ba089
Tanatos <61348601+Saptum@users.noreply.github.com>
2024-01-23 15:43:50 +0200

    added a constructor to generate the apple position

--- src/main/java/Apple.java
+++ src/main/java/Apple.java
@@ -5,4 +5,8 @@ public class Apple {
     public int posY;
     public int posX;
 
+    public void setDandomPosition() {
+        posX = Math.abs((int) (Math.random() * SnakeMain.WIDTH - 1));
+        posY = Math.abs((int) (Math.random() * SnakeMain.HEIGHT - 1));
+    }
 }
