47980032cbaa8774d11bc56e7f81e460c421785f
Tanatos <61348601+Saptum@users.noreply.github.com>
2024-01-23 15:31:28 +0200

    Initial commit

---
+++ src/main/java/SnakeMain.java
@@ -0,0 +1,11 @@
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.KeyAdapter;
+import java.awt.event.KeyEvent;
+
+
+public class SnakeMain extends JPanel implements ActionListener  {
+
+}
