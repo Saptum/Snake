01a0b56c7d1906cb42a1f17e8523b304cee0b295
Tanatos <61348601+Saptum@users.noreply.github.com>
2024-01-23 15:40:35 +0200

    added a constructor to initialize the snake with initial coordinates

--- src/main/java/Snake.java
+++ src/main/java/Snake.java
@@ -6,4 +6,12 @@ public class Snake {
     // Массивы для хранения координат X и Y сегментов змеи
     public int[] snakeX = new int[300];
     public int[] snakeY = new int[300];
+
+    // Конструктор для инициализации змеи начальными координатами (x1, y1) и (x2, y2)
+    public Snake(int x1, int y1, int x2, int y2) {
+        snakeX[0] = x1;
+        snakeX[1] = x2;
+        snakeY[0] = y1;
+        snakeY[1] = y2;
+    }
 }
